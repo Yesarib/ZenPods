f7f9cc162b32ca1ad522cf6bd2cae38f8f10fba3
Yesarib <yesari_yesari@hotmail.com>
2023-09-04 14:59:34 +0300

    Episode card draft

--- client/src/Components/EpisodeCard.jsx
+++ client/src/Components/EpisodeCard.jsx
@@ -3,14 +3,36 @@ import { usePodcastContext } from '../Context/PodcastContext'; // PodcastContext
 
 const EpisodeCard = () => {
   const { nowPlayingPodcast } = usePodcastContext(); // Şu anda çalınan podcast bilgilerini alın
-
+    console.log(nowPlayingPodcast);
 return (
-    <div className='text-white mt-36 text-center'>
+    <div className='text-white mt-36 text-center mb-3'>
         {nowPlayingPodcast ? (
-            <div>
-            <h2>{nowPlayingPodcast.title}</h2>
-            <p>{nowPlayingPodcast.description}</p>
-            {/* Diğer podcast bilgileri */}
+            <div className='flex justify-between items-center text-center ml-2'>
+                <div className='flex'>
+                    <img src={nowPlayingPodcast.imageUrl} alt={nowPlayingPodcast.title} className='w-16' />
+                    <div className='flex flex-col text-start ml-5'>
+                        <h1 className='font-medium '> {nowPlayingPodcast.title} </h1>
+                        <h1 className='font-medium text-gray-400'> {nowPlayingPodcast.description} </h1>
+                    </div>
+                </div>
+
+                <div className='flex flex-col justify-center items-center text-center' >
+                    <div className='flex'>
+                        Medya işaretleri
+                    </div>
+                    <div className='flex text-center items-center space-x-2'>
+                        <div> 0 </div>
+                        <div className="w-96 border-t border-gray-300"></div>
+                        <div>
+                            Süre
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex'>
+                    <div> İşaretler </div>
+                </div>
+
             </div>
         ) : (
             <div>Şu anda çalınan podcast yok</div>
